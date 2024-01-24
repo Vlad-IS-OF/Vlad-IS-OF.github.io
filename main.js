a25ec8376c91b06c89c499f368cb66234c9c656b
Vlad-IS-OF <71877481+Vlad-IS-OF@users.noreply.github.com>
2024-01-24 11:31:54 +0500

    First commit

---
+++ main.js
@@ -0,0 +1,244 @@
+$(document).ready(function() {
+    $("img").addClass("addingGrayScale");
+    // для st1 Андрей Аликин
+    $(".st1").click(function(){
+        if($(".st1").hasClass("addingGrayScale")){
+            //Удалить лишнее содержание
+            $(".div_main_content").find(".delete").remove();
+            //Сообщение
+            $(".edit").html(
+                "<div align='CENTER'>&nbsp;&nbsp;&nbsp;&nbsp;Это студент <B>Андрей Аликин</B> <a align='CENTER' href='https://vk.com/al_andrey'>Страница в ВК</a>"
+                +"<br> Я мало что знаю об этом студенте </div>"
+                )
+            //Раскрасить картинку
+            $("img").addClass("addingGrayScale");
+            $(".st1").removeClass("addingGrayScale");
+        }
+    });
+    // для st2
+    $(".st2").click(function(){
+        if($(".st2").hasClass("addingGrayScale")){
+             //Удалить лишнее содержание
+             $(".div_main_content").find(".delete").remove();
+             //Сообщение
+             $(".edit").html(
+                 "<div align='CENTER'>&nbsp;&nbsp;&nbsp;&nbsp;Это студентка <B>Ксения Арштейн</B> <a align='CENTER' href='https://vk.com/id334276686'>Страница в ВК</a>"
+                 +"<br> Я мало что знаю об этом студенте </div>"
+                 )
+             //Раскрасить картинку
+            $("img").addClass("addingGrayScale");
+            $(".st2").removeClass("addingGrayScale");
+        }
+    });
+    // для st3
+    $(".st3").click(function(){
+        if($(".st3").hasClass("addingGrayScale")){
+            //Удалить лишнее содержание
+            $(".div_main_content").find(".delete").remove();
+            //Сообщение
+            $(".edit").html(
+                "<div align='CENTER'>&nbsp;&nbsp;&nbsp;&nbsp;Это студент <B>Михаил Батюков</B> <a align='CENTER' href='https://vk.com/id201420298'>Страница в ВК</a>"
+                +"<br> Студент отличник, закончил обучение СПО на красный диплом. Занимается спортом и научной деятельностью </div>"
+                )
+            //Раскрасить картинку
+            $("img").addClass("addingGrayScale");
+            $(".st3").removeClass("addingGrayScale");
+        }
+    });
+    // для st4
+    $(".st4").click(function(){
+        if($(".st4").hasClass("addingGrayScale")){
+            //Удалить лишнее содержание
+            $(".div_main_content").find(".delete").remove();
+            //Сообщение
+            $(".edit").html(
+                "<div align='CENTER'>&nbsp;&nbsp;&nbsp;&nbsp;Это студент <B>Илья Субботин</B> <a align='CENTER' href='https://vk.com/id247431800'>Страница в ВК</a>"
+                +"<br> Я мало что знаю об этом студенте </div>"
+                )
+            //Раскрасить картинку
+            $("img").addClass("addingGrayScale");
+            $(".st4").removeClass("addingGrayScale");
+        }
+    });
+    // для st5
+    $(".st5").click(function(){
+        if($(".st5").hasClass("addingGrayScale")){
+             //Удалить лишнее содержание
+             $(".div_main_content").find(".delete").remove();
+             //Сообщение
+             $(".edit").html(
+                 "<div align='CENTER'>&nbsp;&nbsp;&nbsp;&nbsp;Это студент <B>Егор Игонин</B> <a align='CENTER' href='https://vk.com/new_ratka'>Страница в ВК</a>"
+                 +"<br> Очень много спамит в чат группы</div>"
+                 )
+             //Раскрасить картинку
+            $("img").addClass("addingGrayScale");
+            $(".st5").removeClass("addingGrayScale");
+        }
+    });
+    // для st6
+    $(".st6").click(function(){
+        if($(".st6").hasClass("addingGrayScale")){
+            //Удалить лишнее содержание
+            $(".div_main_content").find(".delete").remove();
+            //Сообщение
+            $(".edit").html(
+                "<div align='CENTER'>&nbsp;&nbsp;&nbsp;&nbsp;Это студентка <B>Татьяна Коваленко</B> <a align='CENTER' href='https://vk.com/tanya___kovalenko'>Страница в ВК</a>"
+                +"<br> Я мало что знаю об этом студенте </div>"
+                )
+            //Раскрасить картинку
+            $("img").addClass("addingGrayScale");
+            $(".st6").removeClass("addingGrayScale");
+        }
+    });
+    // для st7
+    $(".st7").click(function(){
+        if($(".st7").hasClass("addingGrayScale")){
+             //Удалить лишнее содержание
+             $(".div_main_content").find(".delete").remove();
+             //Сообщение
+             $(".edit").html(
+                 "<div align='CENTER'>&nbsp;&nbsp;&nbsp;&nbsp;Это студентка <B>Татьяна Лузина</B> <a align='CENTER' href='https://vk.com/twinburg'>Страница в ВК</a>"
+                 +"<br> Староста нашей группы. Закончила обучение СПО с красным дипломом."
+                 +"<br> Участвует в творческой и научной деятельности </div>"
+                 )
+             //Раскрасить картинку
+            $("img").addClass("addingGrayScale");
+            $(".st7").removeClass("addingGrayScale");
+        }
+    });
+    // для st8
+    $(".st8").click(function(){
+        if($(".st8").hasClass("addingGrayScale")){
+             //Удалить лишнее содержание
+             $(".div_main_content").find(".delete").remove();
+             //Сообщение
+             $(".edit").html(
+                 "<div align='CENTER'>&nbsp;&nbsp;&nbsp;&nbsp;Это студент <B>Данил Мальтсев</B> <a align='CENTER' href='https://vk.com/danielm080604'>Страница в ВК</a>"
+                 +"<br> Я мало что знаю об этом студенте. Но был удивлён когда узнал, что он ведёт свою группу в ВК </div>"
+                 )
+             //Раскрасить картинку
+            $("img").addClass("addingGrayScale");
+            $(".st8").removeClass("addingGrayScale");
+        }
+    });
+    // для st9
+    $(".st9").click(function(){
+        if($(".st9").hasClass("addingGrayScale")){
+             //Удалить лишнее содержание
+             $(".div_main_content").find(".delete").remove();
+             //Сообщение
+             $(".edit").html(
+                 "<div align='CENTER'>&nbsp;&nbsp;&nbsp;&nbsp;Это студент <B>Кирил Мазеин</B> <a align='CENTER' href='https://vk.com/mka02'>Страница в ВК</a><"
+                 +"<br> Он закончил СПО с обычным дипломом. Участвует в различных Олимпиадах по программированию </div>"
+                 )
+             //Раскрасить картинку
+            $("img").addClass("addingGrayScale");
+            $(".st9").removeClass("addingGrayScale");
+        }
+    });
+    // для st10
+    $(".st10").click(function(){
+        if($(".st10").hasClass("addingGrayScale")){
+             //Удалить лишнее содержание
+             $(".div_main_content").find(".delete").remove();
+             //Сообщение
+             $(".edit").html(
+                 "<div align='CENTER'>&nbsp;&nbsp;&nbsp;&nbsp;Это студентка <B>Анастасия Помазкина</B> <a align='CENTER' href='https://vk.com/id412485879'>Страница в ВК</a>"
+                 +"<br> Я мало что знаю об этом студенте </div>"
+                 )
+             //Раскрасить картинку
+            $("img").addClass("addingGrayScale");
+            $(".st10").removeClass("addingGrayScale");
+        }
+    });
+    // для st11
+    $(".st11").click(function(){
+        if($(".st11").hasClass("addingGrayScale")){
+            //Удалить лишнее содержание
+            $(".div_main_content").find(".delete").remove();
+            //Сообщение
+            $(".edit").html(
+                "<div align='CENTER'>&nbsp;&nbsp;&nbsp;&nbsp;Это студент <B>Владислав Рыков</B> <a align='CENTER' href='https://vk.com/valdelite'>Страница в ВК</a>"
+                +"<br> Он закончил обучение СПО. Хорошо разбирается в языках программирования </div>"
+                )
+            //Раскрасить картинку
+            $("img").addClass("addingGrayScale");
+            $(".st11").removeClass("addingGrayScale");
+        }
+    });
+    // для st12
+    $(".st12").click(function(){
+        if($(".st12").hasClass("addingGrayScale")){
+            //Удалить лишнее содержание
+            $(".div_main_content").find(".delete").remove();
+            //Сообщение
+            $(".edit").html(
+                "<div align='CENTER'>&nbsp;&nbsp;&nbsp;&nbsp;Это студентка <B>Виктория Сергеевна</B> <a align='CENTER' href='https://vk.com/victoria_sweet02'>Страница в ВК</a>"
+                +"<br> Она закончила обучение СПО. А также занимается маникюром</div>"
+                )
+            //Раскрасить картинку
+            $("img").addClass("addingGrayScale");
+            $(".st12").removeClass("addingGrayScale");
+        }
+    });
+    // для st13
+    $(".st13").click(function(){
+        if($(".st13").hasClass("addingGrayScale")){
+             //Удалить лишнее содержание
+             $(".div_main_content").find(".delete").remove();
+             //Сообщение
+             $(".edit").html(
+                 "<div align='CENTER'>&nbsp;&nbsp;&nbsp;&nbsp;Это студент <B>Владислав Сивков</B> <a align='CENTER' href='https://vk.com/vlad__of'>Страница в ВК</a>"
+                 +"<br> Это я. Разработчик данного сайта. Закончил обучение СПО с красным дипломом."
+                 +"<br> Активно участвую в Творческой и Научной деятельности Института."
+                 +"<br> А также провожу пары для студентов СПО. Обучаю по дисциплине '1С:Предприятие'</div>"
+                 )
+             //Раскрасить картинку
+            $("img").addClass("addingGrayScale");
+            $(".st13").removeClass("addingGrayScale");
+        }
+    });
+    // для st14
+    $(".st14").click(function(){
+        if($(".st14").hasClass("addingGrayScale")){
+             //Удалить лишнее содержание
+             $(".div_main_content").find(".delete").remove();
+             //Сообщение
+             $(".edit").html(
+                 "<div align='CENTER'>&nbsp;&nbsp;&nbsp;&nbsp;Это студент <B>Антон Шавшуков</B> <a align='CENTER' href='https://vk.com/anton162002'>Страница в ВК</a>"
+                 +"<br> Он закончил обучение СПО. Отличный танцор. Активно проявляет творческие инициативы в институте."
+                 +"<br>Является председателем студенческого совета</div>"
+                 )
+             //Раскрасить картинку
+            $("img").addClass("addingGrayScale");
+            $(".st14").removeClass("addingGrayScale");
+        }
+    });
+});
+
+$(function() {
+ 
+    $(window).scroll(function() {
+     
+        if($(this).scrollTop() != 0) {
+        
+            $('#toTop').fadeIn();
+            $('#HOME').fadeIn();
+
+        
+        } else {
+        
+            $('#toTop').fadeOut();
+            $('#HOME').fadeOut();
+        
+        }
+        
+    });
+        
+    $('#toTop').click(function() {
+        
+        $('body,html').animate({scrollTop:0},800);
+        
+    });
+     
+});
